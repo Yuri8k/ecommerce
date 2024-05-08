e4a27d802b615053990a87b02c24730c1da85ef4
Yuri8k <kamuiakira.yo@gmail.com>
2024-05-07 22:20:36 -0300

    new project

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "VAI SE FUDE",
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    {{ name }}
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script>
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    Footer,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <Footer />
+  </div>
+</template>
