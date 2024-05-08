cb5610a0db86b54c0a437270894af497e142c8b3
Yuri8k <kamuiakira.yo@gmail.com>
2024-05-07 23:19:19 -0300

    init project

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,38 @@
 </script>
 
 <template>
-    <div>
-        
-    </div>
+    <header>
+        <nav class="menu-mobile">
+            <ul class="menu-mobile-nav">
+                <li>#</li>
+            </ul>
+        </nav>
+        <span class="open-menu-mobile">
+            <i class="ri-menu-line"></i>
+        </span>
+        <h1 class="logo">LOGO</h1>
+        <form action="">
+            <input type="text" placeholder="Pesquise">
+            <button class="search">
+                <i class="ri-search-line"></i>
+            </button>
+        </form>
+        <div class="my-acount">
+            <span class="my-acount-wishlist">
+                <i class="ri-hearts-line"></i>
+            </span>
+            <span class="my-acount-profile">
+                <i class="ri-account-circle-line"></i>
+            </span>
+            <span class="my-acount-cart">
+                <i class="ri-shopping-cart-line"></i>
+            </span>
+        </div>
+    </header>
 </template>
+
+<style lang="scss">
+    header {
+        background: #212121;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,48 @@
 <script>
 import Footer from "@/components/Footer.vue";
+import Header from "@/components/Header.vue";
 
 export default {
+  data(){
+    return {
+      api: []
+    }
+  },
   components: {
+    Header,
     Footer,
   },
+  methods: {
+    apiRequest() {
+      fetch('https://fakestoreapi.com/products')
+            .then((r) => r.json())
+            .then((r) => {
+              this.api = r;
+              console.log(r)
+            })
+    }
+  },
+  created() {
+    this.apiRequest();
+  }
 };
 </script>
 
 <template>
   <div>
+    <Header />
+    <ul class="product-list">
+      <li class="product-item" v-for="item in api" :key="item.index">
+        <figure>
+          <img :src="item.image" :alt="item.title">
+        </figure>
+        <h3 class="product-item-title">{{ item.title }}</h3>
+        <div class="product-item-content">
+          <span class="product-item-price">{{ item.price }}</span>
+          <span class="product-item-rate">{{ item.rating.rate }}</span>
+        </div>
+      </li>
+    </ul>
     <Footer />
   </div>
 </template>
